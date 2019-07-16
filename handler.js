4bf18ef3f50cb633f779c1a89bfb61f78e32bac2
Bill Barrington <bill.barrington@candidpartners.com>
2019-07-16 07:42:00 -0400

    Still working with the dialog model.

--- handler.js
+++ handler.js
@@ -1,14 +1,14 @@
 'use strict';
 
-
-const Alexa = require('alexa-sdk');
-
+const Alexa = require ('alexa-sdk');
+const APP_ID = 'amzn1.ask.skill.81ca916f-affa-45e2-a213-029ebe407dec';
 
 // -----------------------------------------------------
 
 const states = {
     STARTMODE: '_STARTMODE',
-    POSSELECTMODE: '_POSSELECTMODE'
+    POSSELECTMODE: '_POSSELECTMODE',
+    POSCONFIRMMODE: '_POSCONFIRMMODE'
 };
 
 /* INTENT HANDLERS */
@@ -16,57 +16,74 @@ const states = {
 const NewSessionHandler = {
     'NewSession': function () {
         this.handler.state = states.STARTMODE;
-        this.emit(':tell', 'Welcome to the interactive mad lib generator.')
+        this.emit (':ask', 'Welcome to the interactive mad lib generator. Would you like to play?');
     }
 };
 
 const startModeHandlers =
-    Alexa.CreateStateHandler(states.STARTMODE, {
-        'NewSession': function () {
+    Alexa.CreateStateHandler (states.STARTMODE, {
+        'AMAZON.YesIntent': function () {
+            console.log (`YesIntent: ${JSON.stringify (this.event)}`);
             this.handler.state = states.POSSELECTMODE;
-            this.emit(':ask', 'Please give me a verb.')
+            this.emit (':elicitSlot', 'Please select a verb', 'Please select a verb', intent);
+        },
+
+        'AMAZON.NoIntent': function () {
+            console.log (`NoIntent: ${JSON.stringify (this.event)}`);
+            this.emit(':tell', 'Ok, see you next time!');
+        },
+
+        'NewSession': function () {
+            console.log (`NewSession: ${JSON.stringify (this.event)}`);
+            this.handler.state = states.POSSELECTMODE;
+            const intent = this.event.request.intent;
+            // todo hard-coded to verb for now
+            this.emit (':elicitSlot', 'Please select a verb', 'Please select a verb', intent);
         },
 
         'SessionEndedRequest': function () {
-            console.log('Session ended');
-            this.emit(':tell', 'Your session has ended');
+            console.log ('Session ended');
+            this.emit (':tell', 'Your session has ended');
         },
 
         'Unhandled': function () {
-            console.error(`Unhandled: ${this.handler.state}`);
-            const message = "Sorry, I didn't quite get that one";
-            this.emit(':tell', message);
+            console.error (`Unhandled: ${this.handler.state}`);
+            const message = 'Sorry, I didn\'t quite get that one';
+            this.emit (':ask', message, 'Please try again');
         }
     });
 
 const posSelectModeHandlers =
-    Alexa.CreateStateHandler(states.POSSELECTMODE, {
+    Alexa.CreateStateHandler (states.POSSELECTMODE, {
 
         'PosSelectIntent': function () {
+            console.log (`PosSelectIntent: ${JSON.stringify (this.event)}`);
             const pos = this.event.request.intent.slots.pos.value;
-            console.log(`Received: ${pos}`);
+            console.log (`Received: ${pos}`);
             // todo Hard-code the response
             this.attributes['pos'] = pos;
             this.handler.state = states.STARTMODE;
-            this.emit(':tell', `OK, Thanks, I understood your selection is ${pos}.`)
+            this.emit (':tell', `OK, Thanks, I understood your selection is ${pos}.`);
         },
 
         'Unhandled': function () {
-            console.error(`Unhandled: ${this.handler.state}`);
-            const message = "Sorry, I didn't quite get that one";
-            this.emit(':tell', message);
+            console.error (`Unhandled: ${this.handler.state}`);
+            const message = 'Sorry, I didn\'t quite get that one';
+            this.emit (':tell', message);
         }
     });
 
 
 exports.madLib = function (event, context) {
-    console.log(`REQUEST++++${JSON.stringify(event)}`);
+    console.log (`REQUEST++++${JSON.stringify (event)}`);
 
-    const alexa = Alexa.handler(event, context);
+    const alexa = Alexa.handler (event, context);
 
-    alexa.registerHandlers(NewSessionHandler, startModeHandlers, posSelectModeHandlers);
+    alexa.appId = APP_ID;
 
-    alexa.execute();
+    alexa.registerHandlers (NewSessionHandler, startModeHandlers, posSelectModeHandlers);
+
+    alexa.execute ();
 
 };
 
